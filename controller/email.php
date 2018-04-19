d53d214a448217c3284c51102dd0dd3a729705c2
willianBox <willian@boxti.com.br>
2018-04-19 02:16:46 -0300

    alterado smtp

--- controller/email.php
+++ controller/email.php
@@ -16,8 +16,8 @@ try {
      $mail->Host = 'smtp.gmail.com'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
      $mail->SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
      $mail->Port       = 587; //  Usar 587 porta SMTP
-     $mail->Username = 'willianf4m@gmail.com'; // Usuário do servidor SMTP (endereço de email)
-     $mail->Password = 'silveira1'; // Senha do servidor SMTP (senha do email usado)
+     // $mail->Username = 'usuário de smtp'; // Usuário do servidor SMTP (endereço de email)
+     // $mail->Password = 'senha de smtp'; // Senha do servidor SMTP (senha do email usado)
  
      //Define o remetente
      // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
