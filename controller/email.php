b27d7b53b49dafd82f64967c5886c0a1bb1ac112
willianBox <willian@boxti.com.br>
2018-04-19 01:57:36 -0300

    Criado repositório

---
+++ controller/email.php
@@ -0,0 +1,53 @@
+<?php
+ini_set( 'display_errors', '1');
+print_r($_POST);
+print_r($_GET);
+// Inclui o arquivo class.phpmailer.php localizado na pasta class
+require_once("./lib/PHPMailer/class.phpmailer.php");
+ 
+// Inicia a classe PHPMailer
+$mail = new PHPMailer(true);
+ 
+// Define os dados do servidor e tipo de conexão
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+$mail->IsSMTP(); // Define que a mensagem será SMTP
+ 
+try {
+     $mail->Host = 'smtp.gmail.com'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
+     $mail->SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
+     $mail->Port       = 587; //  Usar 587 porta SMTP
+     // $mail->Username = 'usuário de smtp'; // Usuário do servidor SMTP (endereço de email)
+     // $mail->Password = 'senha de smtp'; // Senha do servidor SMTP (senha do email usado)
+ 
+     //Define o remetente
+     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
+     $mail->SetFrom('willianf4m@gmail.com', 'Nome'); //Seu e-mail
+     // $mail->AddReplyTo('seu@e-mail.com.br', 'Nome'); //Seu e-mail
+     $mail->Subject = 'Assunto';//Assunto do e-mail
+ 
+ 
+     //Define os destinatário(s)
+     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     $mail->AddAddress('willianf4m@gmail.com', 'Teste Locaweb');
+ 
+     //Campos abaixo são opcionais 
+     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     //$mail->AddCC('destinarario@dominio.com.br', 'Destinatario'); // Copia
+     //$mail->AddBCC('destinatario_oculto@dominio.com.br', 'Destinatario2`'); // Cópia Oculta
+     //$mail->AddAttachment('images/phpmailer.gif');      // Adicionar um anexo
+ 
+ 
+     //Define o corpo do email
+     $mail->MsgHTML('corpo do email'); 
+ 
+     ////Caso queira colocar o conteudo de um arquivo utilize o método abaixo ao invés da mensagem no corpo do e-mail.
+     //$mail->MsgHTML(file_get_contents('arquivo.html'));
+ 
+     $mail->Send();
+     echo "Mensagem enviada com sucesso</p>\n";
+ 
+    //caso apresente algum erro é apresentado abaixo com essa exceção.
+    }catch (phpmailerException $e) {
+      echo $e->errorMessage(); //Mensagem de erro costumizada do PHPMailer
+}
+?>
