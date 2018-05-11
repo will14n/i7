4d79838ceb2cd3aed40d7cce4dd7a74b49dec474
willianBox <willian@boxti.com.br>
2018-05-11 02:56:20 -0300

    Arrumado área de atuação e css

--- js/init.js
+++ js/init.js
@@ -1,13 +1,3 @@
 $(document).ready(function(){
-    $('#atuacao').bind('inview', function (event, visible) {
-  alert("teste");
-      if (visible == true) {
-        // element is now visible in the viewport
-         $("h2").addClass('#active');
-          alert('found h2!')
-      } else {
-       $("h2").removeClass('#active');
-        // element has gone out of viewport
-      }
-    });
+  
 });
