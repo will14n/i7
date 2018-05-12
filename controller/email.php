529a43424f26c0bdab1e0bfd89247ec1309f638e
willianBox <willian@boxti.com.br>
2018-05-12 01:53:21 -0300

    teste email

--- controller/email.php
+++ controller/email.php
@@ -13,41 +13,55 @@ $mail = new PHPMailer(true);
 $mail->IsSMTP(); // Define que a mensagem será SMTP
  
 try {
-     $mail->Host = 'smtp.gmail.com'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
-     $mail->SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
-     $mail->Port       = 587; //  Usar 587 porta SMTP
-     // $mail->Username = 'usuário de smtp'; // Usuário do servidor SMTP (endereço de email)
-     // $mail->Password = 'senha de smtp'; // Senha do servidor SMTP (senha do email usado)
+
+	$mail = new PHPMailer;
+	$mail->setFrom('contato@i7.eng.br', 'i7');
+	$mail->addAddress('willianf4m@gmail.com', 'willian');
+	$mail->Subject  = 'First PHPMailer Message';
+	$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+	if(!$mail->send()) {
+	  echo 'Message was not sent.';
+	  echo 'Mailer error: ' . $mail->ErrorInfo;
+	} else {
+	  echo 'Message has been sent.';
+	}
+
+	  /*
+	$mail->Host = 'smtp.gmail.com'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
+	$mail->SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
+	$mail->Port       = 587; //  Usar 587 porta SMTP
+	//$mail->Username = 'usuário de smtp'; // Usuário do servidor SMTP (endereço de email)
+	//$mail->Password = 'senha de smtp'; // Senha do servidor SMTP (senha do email usado)
  
-     //Define o remetente
-     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
-     $mail->SetFrom('willianf4m@gmail.com', 'Nome'); //Seu e-mail
-     // $mail->AddReplyTo('seu@e-mail.com.br', 'Nome'); //Seu e-mail
-     $mail->Subject = 'Assunto';//Assunto do e-mail
+	//Define o remetente
+	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
+	$mail->SetFrom('willianf4m@gmail.com', 'Nome'); //Seu e-mail
+	//$mail->AddReplyTo('seu@e-mail.com.br', 'Nome'); //Seu e-mail
+	$mail->Subject = 'Assunto';//Assunto do e-mail
  
  
-     //Define os destinatário(s)
-     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-     $mail->AddAddress('willianf4m@gmail.com', 'Teste Locaweb');
+	 //Define os destinatário(s)
+	 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+	$mail->AddAddress('willianf4m@gmail.com', 'Teste Locaweb');
  
-     //Campos abaixo são opcionais 
-     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-     //$mail->AddCC('destinarario@dominio.com.br', 'Destinatario'); // Copia
-     //$mail->AddBCC('destinatario_oculto@dominio.com.br', 'Destinatario2`'); // Cópia Oculta
-     //$mail->AddAttachment('images/phpmailer.gif');      // Adicionar um anexo
+	//Campos abaixo são opcionais 
+	//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+	//$mail->AddCC('destinarario@dominio.com.br', 'Destinatario'); // Copia
+	//$mail->AddBCC('destinatario_oculto@dominio.com.br', 'Destinatario2`'); // Cópia Oculta
+	//$mail->AddAttachment('images/phpmailer.gif');      // Adicionar um anexo
  
  
-     //Define o corpo do email
-     $mail->MsgHTML('corpo do email'); 
+	 //Define o corpo do email
+	$mail->MsgHTML('corpo do email'); 
  
-     ////Caso queira colocar o conteudo de um arquivo utilize o método abaixo ao invés da mensagem no corpo do e-mail.
-     //$mail->MsgHTML(file_get_contents('arquivo.html'));
+	 ////Caso queira colocar o conteudo de um arquivo utilize o método abaixo ao invés da mensagem no corpo do e-mail.
+	 //$mail->MsgHTML(file_get_contents('arquivo.html'));
  
-     $mail->Send();
-     echo "Mensagem enviada com sucesso</p>\n";
- 
-    //caso apresente algum erro é apresentado abaixo com essa exceção.
-    }catch (phpmailerException $e) {
-      echo $e->errorMessage(); //Mensagem de erro costumizada do PHPMailer
+	$mail->Send();
+	 echo "Mensagem enviada com sucesso</p>\n";
+ */
+	//caso apresente algum erro é apresentado abaixo com essa exceção.
+}catch (phpmailerException $e) {
+	  echo $e->errorMessage(); //Mensagem de erro costumizada do PHPMailer
 }
 ?>
